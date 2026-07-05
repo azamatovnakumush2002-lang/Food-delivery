e7b485d2f8dca4befefec080ac4847eb4c4aa573
azamatovnakumush2002-lang <azamatovnakumush2002@gmail.com>
2026-07-05 22:18:21 +0500

    Fix folder name

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter } from "react-router-dom";
 import Header from "./sections/header";
 import SectionOne from "./sections/section1";
 import SectionTwo from "./sections/section2";
-import SectionThree from "./sections/sectionthree";
+import SectionThree from "./sections/section3";
 import SectionFour from "./sections/section4";
 import SectionFive from "./sections/section5";
 import SectionSix from "./sections/section6";
